251aa81e662dc6c1a21fd79b7ddc6d9068e2c617
hsantamaria97 <hugosantamaria17@live.com>
2023-04-26 23:52:43 -0500

    t Initial Commit

---
+++ src/main/java/com/databases/project/AutoServiceManagerAPI/AutoServiceManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.databases.project.AutoServiceManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/databases/project/AutoServiceManagerAPI/AutoServiceManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databases.project.AutoServiceManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
